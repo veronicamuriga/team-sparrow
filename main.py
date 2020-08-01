038004882cd2da1562d299e44c8792ed8885e2f9
Veronica Muriga <wairimumuriga@gmail.com>
2020-08-01 19:21:51 -0400

    finally

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from flask import Flask, jsonify, request 
+  
+# # creating a Flask app 
+# app = Flask(__name__) 
+
 import tweepy
 import credentials
 import random 
@@ -8,10 +13,22 @@ auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret
 auth.set_access_token(credentials.access_token, credentials.access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit = True)
 
-
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
+# @app.route('/', methods = ['GET', 'POST']) 
+# def home(): 
+#     if(request.method == 'GET'): 
+  
+#         return "I need a post request!"
+  
+# # A simple function to calculate the square of a number 
+# # the number to be squared is sent in the URL when we use GET 
+# # on the terminal type: curl http://127.0.0.1:5000 / home / 10 
+# # this returns 100 (square of 10) 
+# @app.route('/home/<str:num>', methods = ['GET']) 
+# def disp(num): 
+  
+#     return jsonify({'data': num**2}) 
+  
+  
 
 #usernames are not case sensitive!
 user = 'Jaimeotravez'
@@ -19,43 +36,55 @@ user = 'Jaimeotravez'
 class Game:
     def __init__(self, user_name):
         self.user_name = user_name
+        self.user = api.get_user(self.user_name)
         self.follower_threshold = 1000
         self.friend_threshold = 50
+        self.friend = None
+        self.verified_friends = list()
 
     def find_verified_friends(self, user_name):
-        verified_friends = list()
-        friends = api.friends(user_name)
+        friends = api.friends(self.user_name, count = self.user.friends_count)
         # print(friends)
         for friend in friends:
-            if friend.verified == True and friend.followers_count > self.follower_threshold and friend.protected == False:
-                verified_friends.append(friend.id)
-        return verified_friends
+            # if friend.verified == True and friend.followers_count > self.follower_threshold and friend.protected == False:
+            if friend.verified == True and friend.protected == False:
+                self.verified_friends.append(friend.id)
+        print(self.verified_friends)
 
-    def random_tweet(self):
-        verified_friends = self.find_verified_friends(self.user_name)
-        # print(verified_friends)
 
-        friend_id = random.choice(verified_friends)
-        friend = api.get_user(friend_id)
-        # print(friend.name)
-        status_ix = random.randint(1, min({200, friend.statuses_count}))
+    def get_random_friend(self):
+        self.find_verified_friends(self.user_name)
+        end = min({200, len(self.verified_friends)-1})
+        print(end)
+        rand = random.randint(1, end)
+        print(len(self.verified_friends), rand)
+
+        self.friend = api.get_user(self.verified_friends[rand])
 
         # 200 appears to be the upper bound of tweets we can access at a time
-        tweets = api.user_timeline(friend_id, count = min({200, friend.statuses_count}))
+        self.friend_tweets = api.user_timeline(self.friend.id, count = min({200, self.friend.statuses_count}))
         # print(hasattr(tweets[0], 'retweeted_status'))
 
-        return [friend_id, tweets[status_ix]]
 
-    def random_tweet_wrapper(self):
-        tweet_details = self.random_tweet()
+    def random_tweet_wrapper(self, rt = True):
+        self.get_random_friend()
         #check if tweet is a retweet
-        while hasattr(tweet_details[1], 'retweeted_status') or hasattr(tweet_details[1], 'in_reply_to_status_id'):
-            tweet_details = self.random_tweet()
-        return [tweet_details[0], tweet_details[1].text]
+        while rt:
+            status_ix = random.randint(1, min({200, self.friend.statuses_count}))
+            tweet = self.friend_tweets[status_ix]
+            rt = hasattr(tweet, 'retweeted_status') or tweet.in_reply_to_status_id != None
+
+        return [self.friend.id, tweet.text]
 
 
+
+# # driver function 
+# if __name__ == '__main__': 
+  
+#     app.run(debug = True) 
 gamer = Game(user)
 print(gamer.random_tweet_wrapper())
+
 
 
 
