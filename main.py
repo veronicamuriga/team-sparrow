8595f0b4d73353b6a63cd0da7934feee9a66a38b
Helena Ruiz <hruizrmz@gmail.com>
2020-08-01 12:50:46 -0500

    Update main.py
    fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Tweepy and Twitter API Authentication
 import tweepy
 import credentials
+import games
 auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
 auth.set_access_token(credentials.access_token, credentials.access_secret)
 api = tweepy.API(auth)
@@ -11,4 +12,4 @@ user1 = 'seishun_99'
 user2 = 'rpruiz'
 maxCount = 5000 # Twitter doesn't seem to have a limit, but after 5000 you need to have more followers to keep following
 
-mutuals(user1,user2,maxCount)
+games.mutuals(user1,user2,maxCount) #FollowForFollow
