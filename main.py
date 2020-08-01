7ca6fe4f8b12a78d4277d1d1dde74b7679391564
Veronica Muriga <wairimumuriga@gmail.com>
2020-08-01 11:48:03 -0400

    returning random tweets, though slow

--- main.py
+++ main.py
@@ -14,22 +14,34 @@ public_tweets = api.home_timeline()
 #     print(tweet.text)
 user = 'JaimeOtraVez'
 
+class Game:
+    def __init__(self, user_name):
+        self.user_name = user_name
 
-def find_verified_friends(user_name):
-    verified_friends = set()
-    friends = api.friends('')
-    for friend in friends:
-        if friend.verified == True and friend.followers_count > follower_threshold and friend.protected == False:
-            verified_friends.add(friend)
-    return verified_friends
+    def find_verified_friends(self, user_name):
+        verified_friends = list()
+        friends = api.friends(user_name)
+        # print(friends)
+        for friend in friends:
+            if friend.verified == True and friend.followers_count > follower_threshold and friend.protected == False:
+                verified_friends.append(friend.id)
+        return verified_friends
 
-def random_tweet(verified_friends):
-    friend = random.sample(verified_friends, 1)[0]
-    # max_status_no = friend.statuses_count
-    status_ix = random.randint(1, 20)
-    tweets = api.user_timeline(friend.id)
-    return tweets[status_ix]
-    
+    def random_tweet(self):
+        verified_friends = self.find_verified_friends(self.user_name)
+        # print(verified_friends)
+        friend_id = random.choice(verified_friends)
+        friend = api.get_user(friend_id)
+        status_ix = random.randint(1, min({200, friend.statuses_count}))
+        # 200 appears to be the upper bound of tweets we can access at a time
+        tweets = api.user_timeline(friend_id, count = min({200, friend.statuses_count}))
+        # print(len(tweets))
+        # print(friend.statuses_count, status_ix)
+
+        return tweets[status_ix].text
+
+gamer = Game(user)
+# print(gamer.random_tweet())
 
 
 
