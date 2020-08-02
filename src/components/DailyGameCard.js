1baacd7ccf2d19149b6a5ab6ca57ff46368ba391
jaimecantu <69063285+jaimecantu@users.noreply.github.com>
2020-08-02 06:39:13 -0500

    Changed text to buttons in dashboard

--- src/components/DailyGameCard.js
+++ src/components/DailyGameCard.js
@@ -7,15 +7,15 @@ class DailyGameCard extends React.Component{
             <div className="DailyGameCard">
                 <div className="DailyCards">
                     <div className="card-cover" id="who-said"></div>
-                    <div className="card-text"><p className="daily-accent">#WhoSaidThat?</p></div>
+                    <div className="card-text"><button className="daily-accent">#WhoSaidThat?</button></div>
                 </div>
                 <div className="DailyCards">
                     <div className="card-cover" id="who-liked"></div>
-                    <div className="card-text"><p className="daily-accent">#WhoHasMoreFollowers?</p></div>
+                    <div className="card-text"><button className="daily-accent">#WhoHasMoreFollowers?</button></div>
                 </div>
                 <div className="DailyCards">
                     <div className="card-cover" id="who-follows"></div>
-                    <div className="card-text"><p className="daily-accent">#WhoLikedIt?</p></div>
+                    <div className="card-text"><button className="daily-accent">#WhoLikedIt?</button></div>
                 </div>
             </div>
         );
