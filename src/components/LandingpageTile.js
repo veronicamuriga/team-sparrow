293e5250504cf3af3fe71fd8fa7cd6747023bf64
jaimecantu <69063285+jaimecantu@users.noreply.github.com>
2020-08-02 07:47:02 -0500

    Updated landing page & buttons

--- src/components/LandingpageTile.js
+++ src/components/LandingpageTile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styling/LandingpageTile.css';
 import whitebird from '../images/whitetwitterbird.png';
+import logo from '../img/logo.png';
 import {signInWithTwitter} from '../components/firebase'
 
 class LandingPageTile extends React.Component{
@@ -15,8 +16,9 @@ class LandingPageTile extends React.Component{
             <div className="LandingpageTile">
             	<div className="hero">
 					<div className="hero-text">
+						<div className="logo"><img src={logo}></img></div>
 						<h1>Welcome to <em>#Twivia!</em></h1>
-						<p>Twivia is a .....</p>
+						<p>Game nights will never be the same! Twivia is a personalized Twitter trivia game created by <em>Team Sparrow</em></p>
 						<button onClick={this.signInClicked} className="SignIn"><img src={whitebird}/> <p>Sign in with Twitter</p></button>
 					</div>
 					<div className="hero-media">
